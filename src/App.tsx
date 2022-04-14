399ccd25fea28061357fcab89a95581c293ffe1b
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2022-04-14 11:25:06 +0300

    MicroTasks of 1st week. (Add & delete tasks in todolist)

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,50 @@
-import React, {useState} from 'react';
-import './App.css';
-import {NewComponent} from "./01/NewComponent";
+import React, {useState} from "react";
+import {Tasks} from "./02/components/Tasks";
+import {Input} from "./02/components/Input";
+import {Button} from "./02/components/Button";
 
-export type FilterType = 'ALL' | 'Dollars' | 'RUB'
-
-function App() {
-
-    const money = [
-        {moneyName: 'Dollars', value: 100, number: ' a1234567890'},
-        {moneyName: 'Dollars', value: 50, number: ' z1234567890'},
-        {moneyName: 'RUB', value: 100, number: ' w1234567890'},
-        {moneyName: 'Dollars', value: 100, number: ' e1234567890'},
-        {moneyName: 'Dollars', value: 50, number: ' c1234567890'},
-        {moneyName: 'RUB', value: 100, number: ' r1234567890'},
-        {moneyName: 'Dollars', value: 50, number: ' x1234567890'},
-        {moneyName: 'RUB', value: 50, number: ' v1234567890'}
-    ]
-
-    let [filter, setFilter] = useState<FilterType>('ALL');
-
-    const setFilterHandler = (filterName:FilterType) => {
-        setFilter(filter = filterName)
-    }
-
-    let moneyAfterFilter = money;
-
-    if (filter === 'Dollars') {
-        moneyAfterFilter = money.filter((item) => item.moneyName === 'Dollars')
-    } else if (filter === 'RUB') {
-        moneyAfterFilter = money.filter((item) => item.moneyName === 'RUB')
-    } else if (filter === 'ALL') {
-        moneyAfterFilter = money;
-    }
-
-    return <NewComponent arr={moneyAfterFilter} callback={(filterName) => setFilterHandler(filterName)}/>
+export type TaskType = {
+    id: number
+    title: string
 }
 
-export default App;
+let counter = 4;
+
+const App = () => {
+    const [tasksArr, setTasksArr] = useState<Array<TaskType>>([
+        {id: 1, title: 'Task1'},
+        {id: 2, title: 'Task2'},
+        {id: 3, title: 'Task3'},
+    ])
+
+    const counterID = () => {
+        return counter++;
+    }
+
+    const [word, setWord] = useState<string>('');
+
+    const addWord = (word: string) => {
+        setWord(word);
+    }
+
+    const addTask = () => {
+        setTasksArr([{id: counterID(), title: word}, ...tasksArr]);
+        setWord('');
+    }
+
+    const deleteTask = (id: number) => {
+        setTasksArr(tasksArr.filter(item => item.id !== id))
+    }
+
+    return (
+        <>
+            <div>
+                <Input value={word} callback={addWord}/>
+                <Button title={'add'} callback={addTask}/>
+            </div>
+            <Tasks taskArr={tasksArr} callback={deleteTask}/>
+        </>
+    )
+}
+
+export default App
